a40946b4847f515349296b31b930883f48b93ea6
Filipp Tarkovich <fill.tarkovich@gmail.com>
2022-03-28 13:50:16 +0300

    commit

---
+++ src/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <h2>Made by Filipp</h2>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/Main.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Main = () => {
+  return (
+    <main>
+      <div className="circle">0</div>
+      <div className="buttons">
+        <button>Button text</button>
+        <button>Button text</button>
+        <button>Button text</button>
+        <button>Button text</button>
+        <button>Button text</button>
+      </div>
+    </main>
+  );
+};
+
+export default Main;
