602051bde887dd393ffeb5f050bf30a280a5685f
Filipp Tarkovich <fill.tarkovich@gmail.com>
2022-03-28 14:57:22 +0300

    counter functionality

--- src/Main.js
+++ src/Main.js
@@ -1,18 +1,44 @@
-import React from "react";
+import React, { Component } from "react";
 
-const Main = () => {
-  return (
-    <main>
-      <div className="circle">0</div>
-      <div className="buttons">
-        <button>Button text</button>
-        <button>Button text</button>
-        <button>Button text</button>
-        <button>Button text</button>
-        <button>Button text</button>
-      </div>
-    </main>
-  );
-};
+class Main extends Component {
+  state = {
+    counter: 10,
+  };
+
+  reset = () => {
+    this.setState({ counter: 0 });
+  };
+
+  plusfive = () => {
+    this.setState({ counter: (this.state.counter += 5) });
+  };
+
+  minusfive = () => {
+    this.setState({ counter: (this.state.counter -= 5) });
+  };
+
+  plusone = () => {
+    this.setState({ counter: (this.state.counter += 1) });
+  };
+
+  minusone = () => {
+    this.setState({ counter: (this.state.counter -= 1) });
+  };
+
+  render() {
+    return (
+      <main>
+        <div className="circle">{this.state.counter}</div>
+        <div className="buttons">
+          <button onClick={this.plusone}>+1</button>
+          <button onClick={this.plusfive}>+5</button>
+          <button onClick={this.reset}>Reset</button>
+          <button onClick={this.minusone}>-1</button>
+          <button onClick={this.minusfive}>-5</button>
+        </div>
+      </main>
+    );
+  }
+}
 
 export default Main;
