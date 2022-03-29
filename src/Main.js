da3a2aa7e6b689e6d4637aa05ee02150cb2e5d7e
Filipp Tarkovich <fill.tarkovich@gmail.com>
2022-03-29 14:09:50 +0300

    module styles

--- src/Main.js
+++ src/Main.js
@@ -1,8 +1,10 @@
+/* eslint-disable react/no-direct-mutation-state */
 import React, { Component } from "react";
+import Button from "./Button";
 
 class Main extends Component {
   state = {
-    counter: 10,
+    counter: 0,
   };
 
   reset = () => {
@@ -14,7 +16,9 @@ class Main extends Component {
   };
 
   minusfive = () => {
-    this.setState({ counter: (this.state.counter -= 5) });
+    if (this.state.counter > 4) {
+      this.setState({ counter: (this.state.counter -= 5) });
+    }
   };
 
   plusone = () => {
@@ -22,19 +26,29 @@ class Main extends Component {
   };
 
   minusone = () => {
-    this.setState({ counter: (this.state.counter -= 1) });
+    if (this.state.counter > 0) {
+      this.setState({ counter: (this.state.counter -= 1) });
+    }
   };
 
   render() {
+    let className = "circle";
+    if (this.state.counter % 2) {
+      className += " circle--odd";
+    } else if (this.state.counter === 0) {
+      className = "circle";
+    } else {
+      className += " circle--even";
+    }
     return (
       <main>
-        <div className="circle">{this.state.counter}</div>
+        <div className={className}>{this.state.counter}</div>
         <div className="buttons">
-          <button onClick={this.plusone}>+1</button>
-          <button onClick={this.plusfive}>+5</button>
-          <button onClick={this.reset}>Reset</button>
-          <button onClick={this.minusone}>-1</button>
-          <button onClick={this.minusfive}>-5</button>
+          <Button click={this.plusone}>+1</Button>
+          <Button click={this.plusfive}>+5</Button>
+          <Button click={this.reset}>Reset</Button>
+          <Button click={this.minusfive}>-5</Button>
+          <Button click={this.minusone}>-1</Button>
         </div>
       </main>
     );
